1f4afc90c823649eabb51d3f92808325aab4eed1
Nagarjuna-15 <nagarjunagummalla5@gmail.com>
2024-02-29 17:24:24 -0600

    while loop added

---
+++ src/JavaWork/WhileLoop.java
@@ -0,0 +1,21 @@
+package JavaWork;
+
+
+public class WhileLoop {
+    public static void main(String[] args) {
+        // Initialize a counter variable
+        int counter = 1;
+
+        // Define the condition for the while loop
+        while (counter <= 5) {
+            // Print the current value of the counter
+            System.out.println(counter);
+
+            // Increment the counter by 1 for the next iteration
+            counter++;
+        }
+
+        // The loop will exit when the counter is no longer less than or equal to 5
+    }
+}
+
