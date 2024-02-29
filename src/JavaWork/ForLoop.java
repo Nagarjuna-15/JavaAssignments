1c0d092598f88c70b9069ff82fbd331966148f11
Nagarjuna-15 <nagarjunagummalla5@gmail.com>
2024-02-29 17:19:36 -0600

    for loop added

---
+++ src/JavaWork/ForLoop.java
@@ -0,0 +1,13 @@
+package JavaWork;
+
+public class ForLoop {
+
+        public static void main(String[] args) {
+            // Simple for loop example
+            for (int i = 1; i <= 5; i++) {
+                System.out.println(i);
+            }
+        }
+    }
+
+
